4a3623c680ce3abcd2caad1b6f3bb58cb1ac0c7f
David <40050408+dmeza14@users.noreply.github.com>
2021-07-06 20:13:38 -0600

    version 1.2

---
+++ app/src/main/java/com/example/pruebaproyecto/views/fragments/MainPageFragment.kt
@@ -0,0 +1,10 @@
+package com.example.pruebaproyecto.views.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.pruebaproyecto.R
+
+
+class MainPageFragment : Fragment(R.layout.fragment_main_page) {
+
+
+}
--- app/src/main/java/com/example/pruebaproyecto/views/fragments/SignInFragment.kt
+++ app/src/main/java/com/example/pruebaproyecto/views/fragments/SignInFragment.kt
@@ -2,14 +2,28 @@ package com.example.pruebaproyecto.views.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import com.example.pruebaproyecto.R
 
-class SignInFragment : Fragment() {
+class SignInFragment : Fragment(R.layout.fragment_sign_in) {
+
+    private lateinit var signInButton: Button
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        signInButton = view.findViewById(R.id.signInButton)
+
+        signInButton.setOnClickListener {
+
+            val action = SignInFragmentDirections.actionSignInFragmentToMainPageFragment()
+
+            findNavController().navigate(action)
+
+        }
+
 
     }
 
